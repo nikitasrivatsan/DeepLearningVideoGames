c88a1bc3a2dbcc82d75e675e9eca7e9718be8e70
asrivat1 <akshay.srivatsan@gmail.com>
2015-12-23 16:52:02 -0800

    Cleanup

--- FutureDev/Embed.py
+++
@@ -1,7 +0,0 @@
-import os
-import Tkinter
-
-child_env = dict(os.environ)
-child_env['SDL_WINDOWID'] = the_window_id
-child_env['SDL_VIDEO_WINDOW_POS'] = '{},{}'.format(left, top)
-p = subprocess.Popen([path, width, height], env=child_env)
